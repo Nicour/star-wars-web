bbb163a215288ef959e5506f3b448335c2f9662b
Nicolas Urman <urmannicolas@gmail.com>
2019-07-09 14:51:41 +0200

    add everything

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function LandingPage(parentElement) {
+  this.parentElement = parentElement;
+  this.elements = null;
+}
+
+LandingPage.prototype.generate = function() {
+  this.elements =  `
+    <header>
+      <h1> Super cool Star Wars web site </h1>
+      <h2> Here you can finde the information to become the bes fan of Star Wars </h2>
+    </header>
+  `;
+  this.render();
+}
+
+LandingPage.prototype.render = function() {
+  this.parentElement.innerHTML = this.elements;
+}
